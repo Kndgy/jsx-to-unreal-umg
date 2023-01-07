1b0589b041d85c1c931dc9550c83720bead0d92b
Rafli Ghani <ghanirafli8@gmail.com>
2023-01-07 22:49:49 +0700

    clean files

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
 import './index.css'
 
+const App = () => {
+  return(
+    <div className="App">
+      yeah
+  </div>
+  )
+}
+
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <App/>
 )
