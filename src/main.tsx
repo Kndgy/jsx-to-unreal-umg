4ad24f7b38dc37c21ec49c49fe3fdff46ab3fa46
Rafli Ghani <ghanirafli8@gmail.com>
2023-01-08 15:49:06 +0700

    add empty files to satisfy tsconf

--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,34 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 
-const App = () => {
+function Test() {
   return(
-    <div className="App">
-      yeah
+    <div>
+      Test
+    </div>
+  )
+}
+
+const App = () => {
+  const test = React.createElement("div","children","huh")
+  const reg = /^a*b+[0-9][a-z]/
+  const tested = reg.test('b1aa')
+  if(" "){
+    console.log("a")
+  }
+  // console.log(test)
+  // console.log("jsx element: ", Test())
+
+
+
+  return(
+    <div>
+      <div className='parent test1'>1
+        <div className='child test2'>a</div>
+        2
+        <div className='child test2'>b</div>
+        3
+      </div>
   </div>
   )
 }
