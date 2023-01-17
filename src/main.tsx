ce671bd4fe09016b2a2c0b4c8a9f69181fb2a8b9
Rafli Ghani <ghanirafli8@gmail.com>
2023-01-17 19:05:48 +0700

    restore main.tsx

---
+++ src/main.tsx
@@ -0,0 +1,81 @@
+import React, { ReactNode } from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import { Extract } from './jsxExtractor';
+
+const BasicText = () => {
+  return(
+    //use className to declare widget type, either custom component to support parent name or custom attributes
+    <div className='sizebox'>
+      <div className='textblock'>
+          test text
+      </div>
+      <></>
+    </div>
+  )
+}
+
+const App = () => {
+
+  var sheeth = []
+  sheeth.push(BasicText())
+  // Extract(sheeth)
+  // console.log(Extract(sheeth))
+  // console.log(sheeth)
+
+  // console.log(JSON.stringify(umgScheme.UMGScheme))
+  // console.log(testTemplateString)
+  /* const childs = React.Children.map(<>test</>, (item, index)=> {
+      return(
+        <>{item} string test, index no:{index}</>
+      )
+    })
+    console.log(childs)
+  */
+  // const testJson = [
+  //   {
+  //     type:"firts div",
+  //     props:{
+  //       children:[
+  //         {
+  //           type:"second div",
+  //           props:{
+  //             children:[
+  //               {
+  //                 type:"third div",
+  //                 props:{
+  //                   children:[
+  //                     {
+  //                       type:"fourth div",
+  //                       props:{
+  //                         children:"reached!"
+  //                       }
+  //                     }
+  //                   ]
+  //                 }
+  //               }
+  //             ]
+  //           }
+  //         }
+  //       ]
+  //     }
+  //   }
+  // ]
+  const test = React.createElement("div",{className:'test value', style: {color: "white"}} , "parent", React.createElement("div", null, "what"))
+
+  return(
+    <div>
+      firs line
+      <br/>
+      #
+      {test}
+      #
+      {Extract(sheeth)}
+      <br/>
+    </div>
+  )
+}
+
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  <App/>
+)
