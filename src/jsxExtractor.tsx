b73fa14780b458733e4276b9a0f6a66033e4f18a
Rafli Ghani <ghanirafli8@gmail.com>
2023-01-22 03:13:00 +0700

    wrapepd version of the returned element

--- src/jsxExtractor.tsx
+++ src/jsxExtractor.tsx
@@ -37,7 +37,45 @@ function extractChildrenPropsAndClassName(obj: any, nodes: any[] = []): string[]
   return nodes;
 }
 
+// console.log("direct : ", el)
+console.log("direct : ",extractChildrenPropsAndClassName(el))
 
-console.log(el)
-console.log(extractChildrenPropsAndClassName(el))
+/*
+*/
+
+//same as above function but wrap the children onto the classname as parents
+function extractChildrenPropsAndClassNameWrapped(obj: any, nodes: any = {}) {
+  if (Array.isArray(obj)) {
+      obj.forEach(item => extractChildrenPropsAndClassNameWrapped(item, nodes))
+  } else {
+      if (obj.hasOwnProperty("props")) {
+          if (obj.props.hasOwnProperty("className")) {
+              if(!nodes.hasOwnProperty(obj.props.className)) {
+                  nodes[obj.props.className] = []
+              }
+          }
+          if (obj.props.hasOwnProperty("children")) {
+              if (Array.isArray(obj.props.children)) {
+                  obj.props.children.forEach((child: any) => {
+                      if (typeof child === 'string') {
+                          nodes[obj.props.className].push(child);
+                      } else {
+                        extractChildrenPropsAndClassNameWrapped(child, nodes);
+                      }
+                  });
+              } else if (typeof obj.props.children === 'string') {
+                  nodes[obj.props.className].push(obj.props.children);
+              } else {
+                extractChildrenPropsAndClassNameWrapped(obj.props.children, nodes);
+              }
+          }
+      }
+  }
+  return nodes;
+}
+
+var result = extractChildrenPropsAndClassNameWrapped(el)
+console.log("wrapped in classname : ", result)
+console.log("keys from the object : ", Object.keys(result))
+console.log("value from the object : ", Object.values(result))
 }
