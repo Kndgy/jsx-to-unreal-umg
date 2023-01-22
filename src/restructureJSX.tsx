6fd573ddf9bdf8515217060406f632567bba6f15
Rafli Ghani <ghanirafli8@gmail.com>
2023-01-23 03:03:00 +0700

    make nodejson typesafe

--- src/restructureJSX.tsx
+++ src/restructureJSX.tsx
@@ -1,34 +1,32 @@
 import React from "react";
 
-interface Node {
-  type?: string;
-  className?: string;
+interface NodeJson {
   props?: {
-      children?: Node | Node[];
-  };
-  children?: Node | Node[];
+    className?: string,
+    children?: Node | Node[];
+  }
 }
 
 export function restructureJSON(obj: any) {
-  let newJSON = {
-      props: {
-          className: '',
-          children: []
-      }
-  };
-  function recurse(obj: any, newJSON: any) {
+  let newNode: NodeJson = {
+    props: {
+      className: '',
+      children: []
+    }
+  }
+  function recurse(obj: any, newNode: any) {
       if (Array.isArray(obj)) {
-          obj.forEach(item => recurse(item, newJSON))
+          obj.forEach(item => recurse(item, newNode))
       } else {
           if (obj.hasOwnProperty("props")) {
               if (obj.props.hasOwnProperty("className")) {
-                  newJSON.props.className = obj.props.className;
+                  newNode.props.className = obj.props.className;
               }
               if (obj.props.hasOwnProperty("children")) {
                   if (Array.isArray(obj.props.children)) {
                       obj.props.children.forEach((child: any) => {
                           if (typeof child === 'string') {
-                              newJSON.props.children.push(child);
+                              newNode.props.children.push(child);
                           } else {
                               let newChild = {
                                   props: {
@@ -37,11 +35,11 @@ export function restructureJSON(obj: any) {
                                   }
                               }
                               recurse(child, newChild);
-                              newJSON.props.children.push(newChild);
+                              newNode.props.children.push(newChild);
                           }
                       });
                   } else if (typeof obj.props.children === 'string') {
-                      newJSON.props.children.push(obj.props.children);
+                      newNode.props.children.push(obj.props.children);
                   } else {
                       let newChild = {
                           props: {
@@ -50,12 +48,12 @@ export function restructureJSON(obj: any) {
                           }
                       }
                       recurse(obj.props.children, newChild);
-                      newJSON.props.children.push(newChild);
+                      newNode.props.children.push(newChild);
                   }
               }
           }
       }
   }
-  recurse(obj, newJSON);
-  return newJSON;
+  recurse(obj, newNode);
+  return newNode;
 }
