2da5bf58036cbcdfe3dd6c262f12e0d0ba40ff4b
Rafli Ghani <ghanirafli8@gmail.com>
2023-01-23 03:04:54 +0700

    make parameter to be typesafe aswell

--- src/restructureJSX.tsx
+++ src/restructureJSX.tsx
@@ -1,58 +1,58 @@
 import React from "react";
 
 interface NodeJson {
-  props?: {
-    className?: string,
-    children?: Node | Node[];
+  props: {
+    className: string;
+    children: Array<NodeJson | string>;
   }
 }
 
-export function restructureJSON(obj: any) {
+export function restructureJSON(obj: NodeJson | Array<NodeJson>): NodeJson {
   let newNode: NodeJson = {
     props: {
       className: '',
       children: []
     }
   }
-  function recurse(obj: any, newNode: any) {
-      if (Array.isArray(obj)) {
-          obj.forEach(item => recurse(item, newNode))
-      } else {
-          if (obj.hasOwnProperty("props")) {
-              if (obj.props.hasOwnProperty("className")) {
-                  newNode.props.className = obj.props.className;
-              }
-              if (obj.props.hasOwnProperty("children")) {
-                  if (Array.isArray(obj.props.children)) {
-                      obj.props.children.forEach((child: any) => {
-                          if (typeof child === 'string') {
-                              newNode.props.children.push(child);
-                          } else {
-                              let newChild = {
-                                  props: {
-                                      className: '',
-                                      children: []
-                                  }
-                              }
-                              recurse(child, newChild);
-                              newNode.props.children.push(newChild);
-                          }
-                      });
-                  } else if (typeof obj.props.children === 'string') {
-                      newNode.props.children.push(obj.props.children);
-                  } else {
-                      let newChild = {
-                          props: {
-                              className: '',
-                              children: []
-                          }
-                      }
-                      recurse(obj.props.children, newChild);
-                      newNode.props.children.push(newChild);
+  function recurse(obj: NodeJson | Array<NodeJson>, newNode: NodeJson) {
+    if (Array.isArray(obj)) {
+      obj.forEach(item => recurse(item, newNode))
+    } else {
+      if (obj.hasOwnProperty("props")) {
+        if (obj.props.hasOwnProperty("className")) {
+          newNode.props.className = obj.props.className;
+        }
+        if (obj.props.hasOwnProperty("children")) {
+          if (Array.isArray(obj.props.children)) {
+            obj.props.children.forEach((child: NodeJson | string) => {
+              if (typeof child === 'string') {
+                newNode.props.children.push(child);
+              } else {
+                let newChild = {
+                  props: {
+                    className: '',
+                    children: []
                   }
+                }
+                recurse(child, newChild);
+                newNode.props.children.push(newChild);
               }
+            });
+          } else if (typeof obj.props.children === 'string') {
+            newNode.props.children.push(obj.props.children);
+          } else {
+            let newChild = {
+              props: {
+                className: '',
+                children: []
+              }
+            }
+            recurse(obj.props.children, newChild);
+            newNode.props.children.push(newChild);
           }
+        }
       }
+    }
   }
   recurse(obj, newNode);
   return newNode;
