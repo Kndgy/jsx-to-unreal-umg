4cce9f86549442b3949569a5e5b9d0c7dde9d4d2
Rafli Ghani <ghanirafli8@gmail.com>
2023-01-24 22:29:17 +0700

    return classname as key, children as value, nested

--- src/parser/jsxExtractor.tsx
+++ src/parser/jsxExtractor.tsx
@@ -2,41 +2,54 @@ import React from "react";
 var index = 0;
 
 export function Extract(el:any){
-
-  function extractChildrenPropsAndClassName(obj: any, nodes: any[] = []): string[] {
-    if (Array.isArray(obj)) {
-        obj.forEach(item => extractChildrenPropsAndClassName(item, nodes))
-    } else {
-        if (obj.hasOwnProperty("props")) {
+    
+    function extractChildrenPropsAndClassName(obj: any, parent: any = {}): object {
+        if (Array.isArray(obj)) {
+            obj.forEach(item => extractChildrenPropsAndClassName(item, parent));
+        } else {
+            if (obj.hasOwnProperty("props")) {
             if (obj.props.hasOwnProperty("className")) {
-                nodes.push(obj.props.className);
-                console.log("classname :", obj.props.className)
+                if (!parent.hasOwnProperty(obj.props.className)) {
+                parent[obj.props.className] = {};
+                }
+                parent = parent[obj.props.className];
             }
             if (obj.props.hasOwnProperty("children")) {
                 if (Array.isArray(obj.props.children)) {
-                    obj.props.children.forEach((child: any) => {
-                        if (typeof child === 'string') {
-                            console.log("children string :", child)
-                            nodes.push(child);
-                        } else {
-                            extractChildrenPropsAndClassName(child, nodes);
-                        }
+                obj.props.children.forEach((child: any) => {
+                    if (Array.isArray(child)) {
+                    child.forEach((c: any) => {
+                        extractChildrenPropsAndClassName(c, parent);
                     });
-                } else if (typeof obj.props.children === 'string') {
-                    console.log("children string in obj props :", obj.props.children)
-                    nodes.push(obj.props.children); // return the last children or non props child
+                    } else {
+                    if(child.hasOwnProperty("props")) {
+                        extractChildrenPropsAndClassName(child, parent);
+                    } else {
+                        if(!parent.hasOwnProperty("value")) {
+                        parent["value"] = [child];
+                        } else {
+                        parent["value"].push(child);
+                        }
+                    }
+                    }
+                });
+                } else if (typeof obj.props.children === "string") {
+                parent["value"] = obj.props.children;
                 } else {
-                    extractChildrenPropsAndClassName(obj.props.children, nodes);
+                if (obj.props.children.hasOwnProperty("props")) {
+                    extractChildrenPropsAndClassName(obj.props.children, parent);
+                } else {
+                    parent["value"] = obj.props.children;
+                }
                 }
             }
+            }
         }
+        return parent;
     }
-    return nodes;
-  }
-  // console.log("direct : ",extractChildrenPropsAndClassName(el))
-
-  /*
-  */
+         
+      
+  console.log(extractChildrenPropsAndClassName(el))
 
   //same as above function but wrap the children onto the classname as parents
   function extractChildrenPropsAndClassNameWrapped(obj: any, nodes: any = {}) {
@@ -46,20 +59,15 @@ export function Extract(el:any){
         if (obj.hasOwnProperty("props")) {
             if (obj.props.hasOwnProperty("className")) {
                 if(!nodes.hasOwnProperty(obj.props.className)) {
-                    nodes[obj.props.className] = []
+                    nodes[obj.props.className] = {}
                 }
+                nodes = nodes[obj.props.className]
             }
             if (obj.props.hasOwnProperty("children")) {
                 if (Array.isArray(obj.props.children)) {
                     obj.props.children.forEach((child: any) => {
-                        if (typeof child === 'string') {
-                            nodes[obj.props.className].push(child);
-                        } else {
-                          extractChildrenPropsAndClassNameWrapped(child, nodes);
-                        }
+                        extractChildrenPropsAndClassNameWrapped(child, nodes);
                     });
-                } else if (typeof obj.props.children === 'string') {
-                    nodes[obj.props.className].push(obj.props.children);
                 } else {
                   extractChildrenPropsAndClassNameWrapped(obj.props.children, nodes);
                 }
@@ -67,16 +75,18 @@ export function Extract(el:any){
         }
     }
     return nodes;
-  }
+}
+
+
 
   var result = extractChildrenPropsAndClassNameWrapped(el)
-  // console.log("wrapped in classname : ", result)
+//   console.log("wrapped in classname : ", result)
 
   var resultParent = result
 
-  console.log(result)
+  // console.log(result)
 
-  console.log(el)
+//   console.log(el)
 
   var values: any = []
   var valueRes = Object.values(result).map((items)=>{
@@ -87,34 +97,26 @@ export function Extract(el:any){
     }
   })
 
-  console.log(values)
+  // console.log(values)
 
   const widgetRes = Object.keys(result).map((items)=> {
     var newItems: String[] = []
+    var newitms = items;
     if(items == "sizebox"){
       newItems.push(items)
     return(
-      `
-      Begin Object Class=/Script/UMG.SizeBox 
-      Name=" ${newItems}"
-
-      Begin Object Class=/Script/UMG.SizeBoxSlot 
-        Name="${newItems}_0"
-      End object
-
-      End Object
-      `
+      `Name= "${newItems}"`
     )
-    }else if(items == "textblock"){
+    }else if(items.match(/textblock/g)){
       newItems.push(items)
       return (
-        `content ${newItems} ${values}`.replace(",", "")
+        `content ${newItems}`
         )
     }
   })
 
   console.log(widgetRes)
-  // return widgetRes
+//   return widgetRes
   // return extractChildrenPropsAndClassName(el)
 
 }
