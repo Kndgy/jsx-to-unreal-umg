21628ecc369a0f9a48baf29c5952480636df5054
Rafli Ghani <ghanirafli8@gmail.com>
2023-01-27 23:44:00 +0700

    sizebox component interface props

--- src/sizeBox.tsx
+++ src/sizeBox.tsx
@@ -1,12 +1,14 @@
+import React from "react"
 
 interface sizeBoxProps {
-
+    name?: string;
+    children?: React.ReactNode;
 }
 
-export const sizeBox = () => {
+export const sizeBox = ({name, children}:sizeBoxProps) => {
     return(
         <div className="sizeBox">
-
+            {children}
         </div>
     )
 }
