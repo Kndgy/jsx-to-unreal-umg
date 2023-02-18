7ae29bd3869fefcc2ab31f96b082fe9353443183
kaine <ghanirafli8@gmail.com>
2023-02-18 21:02:59 +0700

    add type, key, and ref to json exrtractor/convert

--- src/parser/restructureJSX.tsx
+++ src/parser/restructureJSX.tsx
@@ -1,67 +1,83 @@
-import React from "react";
-
 interface NodeJson {
+  type: string | Function;
+  key?: string | number | null;
+  ref?: any;
+  props: {
+    className: string;
+    style?: any;
+    children: Array<NodeJson | string>;
+  };
+}
+
+export function convertNodeToJSON(obj: NodeJson | Array<NodeJson>): NodeJson {
+  let newNode: NodeJson = {
+    type: '',
     props: {
-      className: string;
-      style?: any;
-      children: Array<NodeJson | string>;
+      className: '',
+      style: {},
+      children: []
     }
   }
-  
-  export function restructureJSON(obj: NodeJson | Array<NodeJson>): NodeJson {
-    let newNode: NodeJson = {
-      props: {
-        className: '',
-        style: {},
-        children: []
+  function recurse(obj: NodeJson | Array<NodeJson>, newNode: NodeJson) {
+    if (Array.isArray(obj)) {
+      obj.forEach(item => recurse(item, newNode))
+    } else {
+      if (obj.hasOwnProperty("type")) {
+        newNode.type = obj.type;
       }
-    }
-    function recurse(obj: NodeJson | Array<NodeJson>, newNode: NodeJson) {
-      if (Array.isArray(obj)) {
-        obj.forEach(item => recurse(item, newNode))
-      } else {
-        if (obj.hasOwnProperty("props")) {
-          if (obj.props.hasOwnProperty("className")) {
-            newNode.props.className = obj.props.className;
-          }
-          if (obj.props.hasOwnProperty("style")) {
-              newNode.props.style = obj.props.style;
-          }
-          if (obj.props.hasOwnProperty("children")) {
-            if (Array.isArray(obj.props.children)) {
-              obj.props.children.forEach((child: NodeJson | string) => {
-                if (typeof child === 'string') {
-                  newNode.props.children.push(child);
-                } else {
-                  let newChild = {
-                    props: {
-                      className: '',
-                      style: {},
-                      children: []
-                    }
+      if (obj.hasOwnProperty("key")) {
+        newNode.key = obj.key;
+      }
+      if (obj.hasOwnProperty("ref")) {
+        newNode.ref = obj.ref;
+      }
+      if (obj.hasOwnProperty("props")) {
+        if (obj.props.hasOwnProperty("className")) {
+          newNode.props.className = obj.props.className;
+        }
+        if (obj.props.hasOwnProperty("style")) {
+            newNode.props.style = obj.props.style;
+        }
+        if (obj.props.hasOwnProperty("children")) {
+          if (Array.isArray(obj.props.children)) {
+            obj.props.children.forEach((child: NodeJson | string) => {
+              if (typeof child === 'string') {
+                newNode.props.children.push(child);
+              } else {
+                let newChild = {
+                  type: '',
+                  key: '',
+                  ref: '',
+                  props: {
+                    className: '',
+                    style: {},
+                    children: []
                   }
-                  recurse(child, newChild);
-                  newNode.props.children.push(newChild);
                 }
-              });
-            } else if (typeof obj.props.children === 'string') {
-              newNode.props.children.push(obj.props.children);
-            } else {
-              let newChild = {
-                props: {
-                  className: '',
-                  style: {},
-                  children: []
-                }
+                recurse(child, newChild);
+                newNode.props.children.push(newChild);
               }
-              recurse(obj.props.children, newChild);
-              newNode.props.children.push(newChild);
+            });
+          } else if (typeof obj.props.children === 'string') {
+            newNode.props.children.push(obj.props.children);
+          } else {
+            let newChild = {
+              type: '',
+              key: '',
+              ref: '',
+              props: {
+                className: '',
+                style: {},
+                children: []
+              }
             }
+            recurse(obj.props.children, newChild);
+            newNode.props.children.push(newChild);
           }
         }
       }
     }
-    recurse(obj, newNode);
-    return newNode;
   }
-  
+  recurse(obj, newNode);
+  return newNode;
+}
