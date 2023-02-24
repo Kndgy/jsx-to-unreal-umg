5c699dd20a89247c7bd23a0e8d7fd22601d36998
kaine <ghanirafli8@gmail.com>
2023-02-24 23:38:53 +0700

    textarea and result

--- src/modules/editor.tsx
+++ src/modules/editor.tsx
@@ -1,6 +1,15 @@
+import { useState } from 'react'
 import styles from './editor.module.css'
 
 export const Editor = () => {
+
+    const [text, setText] = useState('')
+
+    const handleText = (event:any) => {
+        setText(event.target.value);
+        console.log(event.target.value)
+    }
+
     return(
         <div className={styles.editor}>
             <div className={styles.sideBar}>
@@ -13,10 +22,15 @@ export const Editor = () => {
             </div> 
             <div className={styles.content}>
                 <div className={styles.inputContainer}>
-                    <textarea className={styles.input}/>
+                    <textarea 
+                        className={styles.input}
+                        id='text'
+                        value={text}
+                        onChange={handleText}
+                    />
                 </div>
                 <div className={styles.result}>
-                    result
+                    {text}
                 </div>
             </div>
         </div>
