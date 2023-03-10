6d4d90bfcbd839da6884e2bbe74ebb5b1bdf19b9
kaine <ghanirafli8@gmail.com>
2023-03-11 01:18:16 +0700

    basic drag n drop components

--- src/modules/componentsBar/componentsBar.tsx
+++ src/modules/componentsBar/componentsBar.tsx
@@ -17,55 +17,71 @@ type ComponentProps = {
 const ComponentList: ComponentProps[] = [
     {
         index: 0, 
-        children:<>sizeBox</>
+        children:<div className={styles.dragEl}>sizeBox</div>
     },
     {
         index: 1, 
-        children:<>TextBlock</>
+        children:<div className={styles.dragEl}>TextBlock</div>
     },
     {
         index: 2, 
-        children:<>text test</>
+        children:<div className={styles.dragEl}>text test</div>
     } 
 ]
 
 export const ComponentsBar = () => {
-    const [componentList, setComponentList] = useState(ComponentList);
     const [draggedIndex, setDraggedIndex] = useState(-1);
+    const [Droped, setDroppedIndex ] = useState(-1)
   
     const onDragStart = (index:number) => (e: React.DragEvent<HTMLDivElement>) => {
-      console.log(index)
-    //   setDraggedIndex(index);
+        console.log(index)
+        setDraggedIndex(index);
     };
   
     const onDrop = (e: React.DragEvent<HTMLDivElement>) => {
         console.log("hello")
+        setDroppedIndex(draggedIndex)
     };
 
     const Top = () => {
-      return (
-        <div className={styles.Top}>
-          {componentList.map((component, index) => (
-            <div key={index} draggable="true" onDragStart={onDragStart(index)}>
-              {component.children}
+        return (
+            <div className={styles.Top}>
+                {ComponentList.map((component, index) => (
+                    <div key={index} draggable="true" onDragStart={onDragStart(index)}>
+                        {component.children}
+                    </div>
+                ))}
             </div>
-          ))}
-        </div>
-      );
+        );
     };
   
     const Bottom = () => {
-      return (
-        <div onDrop={onDrop} onDragOver={e => e.preventDefault()}>
-          bottom
-          </div>
-      );
+        if (Droped === -1) {
+            return (
+                <div onDrop={onDrop} onDragOver={e => e.preventDefault()}>
+                    drop here    
+                </div>
+            )
+        }
+        return (
+            <div onDrop={onDrop} onDragOver={e => e.preventDefault()}>
+                drop here    
+                <br/>
+                indexes
+                <br/>
+                {Droped}
+                <br/>
+                components
+                <br/>
+                {ComponentList[Droped].children}
+            </div>
+        );
     }
-  
+
     return (
-      <div className={styles.parent}>
-        <Top />
-        <Bottom />
-      </div>
+        <div className={styles.parent}>
+            <Top />
+            <Bottom />
+        </div>
     );
   }
