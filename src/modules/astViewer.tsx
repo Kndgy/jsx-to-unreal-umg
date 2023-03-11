c7fb1cfedad60f180ec5369ac1cede4b86722612
kaine <ghanirafli8@gmail.com>
2023-03-11 21:48:22 +0700

    separate ast viewer components

--- src/modules/astViewer.tsx
+++ src/modules/astViewer.tsx
@@ -1,14 +1,36 @@
+import { useState } from "react";
 import { convertNodeToJSON } from "../parser/convertNodeToJSON"
 import { ParseStringtoNode } from "../parser/ParseStringtoNode"
+import styles from './astViewer.module.css'
 
-interface astViewer {
-    text: string;
-}
+export const AstViewer = () => {
 
-export const AstViewer = ({text}:astViewer) => {
+    const [text, setText] = useState({
+        text:`<div style={{color:"black", backgroundColor:"white"}} className="classname1">this is text <div> siblings <div> first siblings nested <div> first siblings second nested </div> </div> test </div> last<i>second siblings</i>Another example</div>`,
+    })
+
+    const handleText = (event:any) => {
+        setText({text:event.target.value});
+        console.log(event.target.value)
+    }
+       
+    const reactElements = ParseStringtoNode(text.text);
+    
     return(
-        <div>
-            <pre>{JSON.stringify(convertNodeToJSON(ParseStringtoNode(text)), null , 2)}</pre>
+        <div className={styles.container}>
+            {/* {reactElements} */}
+            <div className={styles.inputContainer}>
+                    <textarea 
+                        className={styles.input}
+                        id='text'
+                        value={text.text}
+                        onChange={handleText}
+                    />
+                </div>
+                <div className={styles.result}>
+                    test
+                    <pre>{JSON.stringify(convertNodeToJSON(ParseStringtoNode(text.text)), null , 2)}</pre>
+                </div>
         </div>
     )
 }
