da1956c13c7df075a7f0cf1fb75e7a1691a38b0f
kaine <ghanirafli8@gmail.com>
2023-03-13 21:51:20 +0700

    simple function that parse parent and children, work indefinitely nested

--- src/main.tsx
+++ src/main.tsx
@@ -23,119 +23,94 @@ const BasicText = () => {
   )
 }
 
-let jsonStruct = {
-  "type": "div",
-  "props": {
-    "className": "sizebox",
-    "style": {
-      "color": "black",
-      "backgroundColor": "white"
-    },
-    "children": [
-      {
-        "type": "div",
-        "key": "2",
-        "ref": null,
-        "props": {
-          "className": "textblock",
-          "style": {},
-          "children": [
-            "content goes here"
-          ]
-        }
-      }
-    ]
-  },
-  "key": "1"
+
+function parseToUmg(jsonStruct: any): string {
+  let widgetType = jsonStruct.type;
+  let name = jsonStruct.props.name;
+  let childrenString = jsonStruct.props.children;
+
+  if (jsonStruct.type === "SizeBox") {
+    widgetType = "SizeBoxSlot";
+    name = jsonStruct.props.name;
+
+    childrenString = parseChildrenToUmg(jsonStruct.props.children);
+    
+    return `
+      Begin Object Class=/Script/UMG.SizeBox Name="${name}"
+
+      Begin Object Class=/Script/UMG.SizeBoxSlot Name="SizeBoxSlot_0"
+      End Object
+
+      Begin Object Name="SizeBoxSlot_0"
+        Parent=/Script/UMG.SizeBox'"${name}"'
+        Content=/Script/UMG.TextBlock'"${jsonStruct.props.children[0].props.name}"'
+      End Object
+
+      Slots(0)=/Script/UMG.SizeBoxSlot'"SizeBoxSlot_0"'
+      bExpandedInDesigner=True
+      DisplayLabel="${name}"
+
+      End Object
+
+      Begin Object Class=/Script/UMGEditor.WidgetSlotPair Name="WidgetSlotPair_0"
+        WidgetName='${name}'
+      End Object
+
+      ${childrenString}
+    `;
+  }
+  else if (jsonStruct.type === "TextBlock") {
+    name = jsonStruct.props.name;
+    const content = jsonStruct.props.children[0];
+    
+    return `
+      Begin Object Class=/Script/UMG.TextBlock Name="${name}"
+        Text=NSLOCTEXT("UMG", "TextBlockDefaultValue", "${content}")
+        DisplayLabel='${name}'
+      End Object
+    `;
+  }
+
+  return "";
 }
 
-let expectedString = `
-  Begin Object Class=/Script/UMG.SizeBox Name="SizeBox_Name"
 
-  //initial slot ok to hardcode
-  Begin Object Class=/Script/UMG.SizeBoxSlot Name="SizeBoxSlot_0"
-  End Object
+function parseChildrenToUmg(children: any[]): string {
+  let childrenString = "";
+  children.forEach((child, index) => {
+    childrenString += `
+      ${parseToUmg(child)}
+    `;
+  });
 
-  //initialize slot, ok to hardcode but not the childrens
-  Begin Object Name="SizeBoxSlot_0"
-    Parent=/Script/UMG.SizeBox'"SizeBox_Name"'//current Parent
-    Content=/Script/UMG.TextBlock'"TextBlock_Name"'//children, possibly all of the nested children
-  End Object
-
-  //properties | props of the slot of the widget
-  Begin Object
-  Slots(0)=/Script/UMG.SizeBoxSlot'"SizeBoxSlot_0"'
-  bExpandedInDesigner=True
-  DisplayLabel="SizeBox_Name"
-  End Object
-
-  //pair, ok to hardcode
-  Begin Object Class=/Script/UMGEditor.WidgetSlotPair Name="WidgetSlotPair_1"
-  WidgetName="SizeBox_Name"
-  End Object
-
-  //first children
-  Begin Object Class=/Script/UMG.TextBlock Name="TextBlock_Name"
-  Text=NSLOCTEXT("UMG", "TextBlockDefaultValue", "Text Block")
-  DisplayLabel="TextBlock_Name"
-
-  End Object
-`
-
-function parseToUmg(){
-
-  let name:string;
-  let widgetType:string;
-
-  let umgStr = `
-    Begin Object
-
-    Begin Object Slot
-    EndObject
-
-    Begin Object Name=" ${widgetType='SizeBoxSlot'}_0"
-     parent=/Script/UMG.SizeBox'"${name="SizeBox_Name"}"'
-     content=/Script/UMG.TextBlock'"${name="TextBlock_Name"}"'
-    End Object
-
-    Begin Object
-    Slots(0)=/Script/UMG.${widgetType='SizeBoxSlot'}'"${widgetType='SizeBoxSlot'}_0"'
-    bExpandedInDesigner=True
-    DisplayLabel="${widgetType='SizeBox_Name'}"
-    End Object
-
-    Begin Object Class=/Script/UMGEditor.WidgetSlotPair Name="WidgetSlotPair_1"
-    WidgetName="${name="SizeBox_Name"}"
-    End Object
-
-    Begin Object Class=/Script/UMG.TextBlock Name="${name="TextBlock_Name"}"
-    Text=NSLOCTEXT("UMG", "TextBlockDefaultValue", "Content")
-    DisplayLabel="${name="TextBlock_Name"}"
-
-    End Object
-  `
-
-  return umgStr
+  return childrenString;
 }
 
-console.log(parseToUmg())
-console.log( <SizeBox name='test' children={<TextBlock children='test'/>}/> )
-// console.log(<TextBlock children='test'/>)
-console.log(BasicText())
+const structure = convertNodeToJSON(
+  <SizeBox name='sizebox_parentParent' children={
+    <SizeBox name='SizeBox_Parent' 
+    children={<SizeBox name='SizeBox_Name' 
+      children={
+        <TextBlock name='TextBlock_Name' children='test'/>}
+        />}
+      />}
+    />
+  )
+console.log(parseToUmg(structure))
 
 const App = () => {
 
   // console.log(convertNodeToJSON(BasicText()))
   // console.log(createReactElement(convertNodeToJSON(BasicText())))
   // console.log(JSON.stringify(Extract(BasicText())))
-  // console.log(SizeBox({name: "name", children:<div>test</div>}))
   return(
     <div className='main'>
       {/* <Editor/> */}
+      result is in console log
       <pre>
         {JSON.stringify(
           convertNodeToJSON(
-            <SizeBox name='SizeBox_Name' children={<TextBlock name='TextBlock_Name' children='test'/>}/>
+            <SizeBox name='sizebox_parentParent' children={<SizeBox name='SizeBox_Parent' children={<SizeBox name='SizeBox_Name' children={<TextBlock name='TextBlock_Name' children='test'/>}/>} />} />
             ),
             null, 
             2
